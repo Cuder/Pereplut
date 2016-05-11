788fc327f14b4c855919971f27ad5925e5dc4455
Nikita Kovin <me@nikitakovin.ru>
2016-05-11 19:10:34 +0300

    Check if an uploaded archive contains an H&M project

--- model/unzipFile.php
+++ model/unzipFile.php
@@ -2,7 +2,20 @@
 $zip = new ZipArchive();
 
 if ($zip->open($target_file)) {
-
+	// Listing all files in archive to find an H&M project
+	$foundProject = false;
+	for ($i = 0; $i < $zip->numFiles; $i++) {
+		if (strpos($zip->getNameIndex($i),".hmxp") !== false) {
+			$foundProject = true;
+			break;
+		}
+	}
+	if ($foundProject == false) {
+		unlink($target_file);
+		$_SESSION['errorCode'] = "noHMXP";
+		fallback();
+	}
+	
 	$unzipDir = $target_dir.basename($_FILES["fileToUpload"]["name"],".".$fileExtension);
 	if (file_exists($unzipDir)) {
 		require_once $rootdir."/libs/removeDir.php";
