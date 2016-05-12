9997c1ef28b86f5ffc48ff53c82c3d1158ce38f4
Nikita Kovin <me@nikitakovin.ru>
2016-05-12 21:03:12 +0300

    Fixed indents

--- core.php
+++ core.php
@@ -1,40 +1,43 @@
 <?php
 // Setting debug level
 if ($debugging == true) {
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
 }
 // If register_globals is turned off, extract super globals (php 4.2.0+)
 if (ini_get('register_globals') != 1) {
-    $supers = array("_REQUEST","_ENV","_SERVER","_POST","_GET","_COOKIE","_SESSION","_FILES","_GLOBALS");
-    foreach ($supers as $__s) {
-        if ((isset($$__s) == true) && (is_array($$__s) == true)) extract($$__s, EXTR_OVERWRITE);
-    }
-    unset($supers);
+	$supers = array("_REQUEST", "_ENV", "_SERVER", "_POST", "_GET", "_COOKIE", "_SESSION", "_FILES", "_GLOBALS");
+	foreach ($supers as $__s) {
+		if ((isset($$__s) == true) && (is_array($$__s) == true)) extract($$__s, EXTR_OVERWRITE);
+	}
+	unset($supers);
 }
 // Prevent any possible XSS attacks via $_GET.
 if (stripget($_GET)) {
-    $_SESSION['errorCode'] = "xss";
-    fallback();
+	$_SESSION['errorCode'] = "xss";
+	fallback();
 }
-function stripget($check_url) {
-    $return = false;
-    if (is_array($check_url)) {
-        foreach ($check_url as $value) {
-            if (stripget($value) == true) {
-                return true;
-            }
-        }
-    } else {
-        $check_url = str_replace(array("\"", "\'"), array("", ""), urldecode($check_url));
-        if (preg_match("/<[^<>]+>/i", $check_url)) {
-            return true;
-        }
-    }
-    return $return;
+function stripget($check_url)
+{
+	$return = false;
+	if (is_array($check_url)) {
+		foreach ($check_url as $value) {
+			if (stripget($value) == true) {
+				return true;
+			}
+		}
+	} else {
+		$check_url = str_replace(array("\"", "\'"), array("", ""), urldecode($check_url));
+		if (preg_match("/<[^<>]+>/i", $check_url)) {
+			return true;
+		}
+	}
+	return $return;
 }
+
 // Fallback to safe area in event of unauthorised access
-function fallback($location = "") {
-    header("Location: http://".$_SERVER['SERVER_NAME']."/".$location);
-    exit;
+function fallback($location = "")
+{
+	header("Location: http://".$_SERVER['SERVER_NAME']."/".$location);
+	exit;
 }
